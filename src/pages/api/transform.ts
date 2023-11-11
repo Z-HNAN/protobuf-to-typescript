fead5f0017bf3b7f70341b151b3fe52d096285aa
Z-HNan <zhn68033@163.com>
2023-11-11 22:44:58 +0800

    feat: trick

--- src/pages/api/transform.ts
+++ src/pages/api/transform.ts
@@ -6,7 +6,7 @@ import { $ } from "zx";
 import { v4 as uuidv4 } from "uuid";
 import _ from 'lodash';
 
-const FALSE = Boolean(1-1); // skip-tree-shanking
+const FALSE = (0 > 1); // skip-tree-shanking
 if (FALSE) {
   import(/* webpackInclude: /ts-proto/ */ 'ts-proto'); // trick keep install ts-proto
 }
